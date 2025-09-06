30d1ab4f06718c867f6a76bf80f1d14589219000
Darozco02 <orozcoperezdaniel82@gmail.com>
2025-09-05 22:56:21 -0600

    Actualizacion proyecto
    Se actualizo el proyecto utilizando las estructuras de repeticion.

--- p01_control_Inventario.py
+++ p01_control_Inventario.py
@@ -1,43 +1,55 @@
 print("Bienvenido al Sistema de control de inventario!")
 print("-" * 50)
 
-# Solicitar al usuario que ingrese los datos del producto 1
-nombre_producto = input("Por favor ingrese el nombre del producto: ").title().strip()
-cantidad = int(input("Por favor ingrese la cantidad de productos: "))
-precio_unitario = float(input("Por favor ingrese el precio del producto: "))
-
-# Solicitar al usuario que ingrese los datos del producto 2
-nombre_producto2 = input("Por favor ingrese el nombre del producto: ").title().strip()
-cantidad2 = int(input("Por favor ingrese la cantidad de productos: "))
-precio_unitario2 = float(input("Por favor ingrese el precio del producto: "))
-
-# Solicitar al usuario que ingrese los datos del producto 3
-nombre_producto3 = input("Por favor ingrese el nombre del producto: ").title().strip()
-cantidad3 = int(input("Por favor ingrese la cantidad de productos: "))
-precio_unitario3 = float(input("Por favor ingrese el precio del producto: "))
-
 #Creacion de listas
-lista_producto1 = [nombre_producto, cantidad, precio_unitario]
-lista_producto2 = [nombre_producto2, cantidad2, precio_unitario2]
-lista_producto3 = [nombre_producto3, cantidad3, precio_unitario3]
-lista_mixta = [lista_producto1, lista_producto2, lista_producto3]
+lista_productos = []
 
-# Calculo del total del inventario
-valor_producto1 = cantidad * precio_unitario
-valor_producto2 = cantidad2 * precio_unitario2
-valor_producto3 = cantidad3 * precio_unitario3
-calculo_total = valor_producto1 + valor_producto2 + valor_producto3
+#Menu
+while True:
+    opc = int(input("Por favor ingrese la opcion deseada: \n1. Ingresar nuevo producto \n2. Ver inventario \n3. Eliminar producto \n 4. Salir \n"))
 
-#Creacion de diccionario
-inventario_total = {
-      "Producto 1": lista_producto1,
-      "Producto 2": lista_producto2,
-      "Producto 3": lista_producto3,
-      "calculo_total": calculo_total
-}
+    #Agregar Productos al Inventario
+    if opc == 1:
+        
+        while True:
+            #Solicitar los datos del producto
+            nombre_producto = input("Por favor ingrese el nombre del producto: ").title().strip()
+            cantidad = int(input("Por favor ingrese la cantidad de productos: "))
+            precio_unitario = float(input("Por favor ingrese el precio del producto: "))
+            lista_productos.append((nombre_producto, cantidad, precio_unitario))
+            print("Producto ingresado con exito!")
+            print("-" * 50)
 
-#Impresion del inventario
-print(f"El inventario total es de: \n{inventario_total['Producto 1']}")
-print(f"{inventario_total['Producto 2']}")
-print(f"{inventario_total['Producto 3']}")
-print(f"El costo total del inventario ingresado es de: {inventario_total['calculo_total']}")
+            #Ingresar nuevo producto
+            nuevo_producto = input("Desea ingresar otro producto? (s/n): ").title().strip()
+            if nuevo_producto != "S":
+                break
+
+    #Ver inventario
+    elif opc == 2:
+        for indice, producto in enumerate(lista_productos, 1):
+            print(f"Producto {indice}: Nombre: {producto[0]}, Cantidad: {producto[1]}, Precio Unitario: {producto[2]} Colones")
+            #Realizar el calculo total por producto
+            calculo_total = producto[1] * producto[2]
+            print(f"El valor total del inventario de {producto[0]} es: {calculo_total} Colones")
+            
+        else:
+            print("El inventario esta vacio, por favor ingrese un producto.")
+            continue
+
+    #Eliminar productos
+    elif opc == 3:
+        producto_eliminar = input("Digite el nombre del producto que desea eliminar: ").title().strip()
+        for producto in lista_productos:
+            if producto_eliminar == producto[0]:
+                lista_productos.remove(producto)
+                print(f"✅ El producto {producto_eliminar} ha sido eliminado del inventario con exito!")
+
+    #Salir del sistema
+    elif opc == 4:
+        print("Gracias por usar el sistema de control de inventario, hasta pronto! 👋")
+        break
+
+    else:
+        print("Opcion no valida, por favor intente de nuevo.")
+        continue
